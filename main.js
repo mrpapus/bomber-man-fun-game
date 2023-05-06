3807178b087067365e2ae54c0d50a87c6273fc50
mrpapus <srednalf05@icloud.com>
2023-05-05 23:10:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -17,24 +17,31 @@ let element = true
 Character();
 floorcolors("greenyellow", "chartreuse");
 walls();
+randomBarrier()
 blackGrid()
 
 //swap levels
 function levelType(){
   //make grass
 if (element === false){
+
   Character();
   floorcolors("greenyellow", "lawngreen");
   walls();
+  randomBarrier()
   blackGrid()
+  
 element = true
 
 } else if(element === true){
 //make ice
+
   Character();
   walls();
   floorcolors("powderblue", "skyblue");
+  randomBarrier()
   blackGrid()
+  
   element = false
 }
 }
@@ -101,13 +108,54 @@ for (let n = 50; n <= 850; n += 50) {
 }
 }
 
-barrierDesign(0,0)
+
+//make a random barrier across the map
+
+
+function randomBarrier(){
+  let randnum = Math.random()
+
+   for (let n = 100; n <= 750; n += 100) {
+    for (let i = 50; i <= 800; i += 50) {
+      let randnum = Math.random()
+if(randnum < 0.7){
+  barrierDesign(n,i)
+}
+    }
+  }
+
+  for (let n = 0; n <= 800; n += 100) {
+    for (let i = 50; i <= 750; i += 50) {
+      let randnum = Math.random()
+    if(randnum < 0.7){
+        barrierDesign(i,n)
+}
+    }
+  }
+}
+
+
+
+
+
+//barrier
+
 
 function barrierDesign(x,y){
-borderwalls(x,y,"	gainsboro",50,50)
-borderwalls(x,y +48,"black",50,2)
-
-
+  borderwalls(x,y,"	gainsboro",50,50)
+  borderwalls(x, y, "white", 3, 50);
+borderwalls(x + 2, y, "white", 2, 47);
+borderwalls(x, y, "white", 50, 3);
+borderwalls(x + 30,y+ 30,"black",20,2)
+borderwalls(x + 40,y+ 40,"black",10,2)
+borderwalls(x + 40,y+ 20,"black",2,30)
+borderwalls(x + 10,y+ 10,"black",2,40)
+borderwalls(x + 10,y+ 40,"black",20,2)
+borderwalls(x,y+ 20,"black",30,2)
+borderwalls(x+30,y,"black",2,22)
+borderwalls(x+20,y+10,"black",20,2)
+borderwalls(x, y + 47, "black", 50, 3);
+borderwalls(x + 5, y + 45, "black", 42, 2);
 
 }
 
